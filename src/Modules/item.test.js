d791b52fefb3414784a50971495c61e4cac9444b
francksefu <90141102+francksefu@users.noreply.github.com>
2023-03-24 05:14:27 +0200

    fix linter error

--- src/Modules/item.test.js
+++ src/Modules/item.test.js
@@ -2,24 +2,23 @@
  * @jest-environment jsdom
  */
 
-import compteur from "./compteurItem";
+import compteur from './compteurItem.js';
 
 test('add something', () => {
-    for (let i = 0; i < 6; i += 1) {
-        document.body.innerHTML += ''
-          + '<div class="col-md-4 mt-3 mb-3">'
+  for (let i = 0; i < 6; i += 1) {
+    document.body.innerHTML += '<div class="col-md-4 mt-3 mb-3">'
           + '<div class="card">'
-          + '<img src="${f[i].strMealThumb}" class="card-img-top" alt="...">'
+          + '<img src="picture" class="card-img-top" alt="...">'
           + '<div class="card-body">'
-          + '<h5 class="card-title">${f[i].strMeal}</h5>'
-          + '<p class="card-text"><span id="${f[i].strMeal}" class="material-symbols-outlined">'
+          + '<h5 class="card-title">name</h5>'
+          + '<p class="card-text"><span id="" class="material-symbols-outlined">'
           + ' favorite'
-          + '</span><br>  ${thinker(likes, f[i].strMeal)} likes </p>'
+          + '</span><br>  thinker(likes, f[i].strMeal)} likes </p>'
           + '<a href="#" class="btn btn-success" >Comment</a>'
           + '</div>'
           + '</div>'
           + '</div>';
-        }
-        const span = document.querySelectorAll('span');
-    expect(compteur(span)).toBe(6);
-  });
+  }
+  const span = document.querySelectorAll('span');
+  expect(compteur(span)).toBe(6);
+});
