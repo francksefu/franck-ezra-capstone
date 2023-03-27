8fc35e3f49005eff03fbe51017d69d5c6052b5a3
Ezra <ezraermy@gmail.com>
2023-03-27 20:03:21 +0300

    Delete inline comment

--- src/index.js
+++ src/index.js
@@ -81,40 +81,6 @@ async function received() {
   complete(data, scoreNameL);
 }
 
-window.openPopup = async function openPopup(id) {
-  const requestUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-  const request = new Request(requestUrl);
-  const response = await fetch(request);
-  const mealDetails = await response.json();
-  const meal = mealDetails.meals[0];
-
-  const popupHTML = `
-    <div class="popup">
-      <div class="popup-content">
-        <img src="${meal.strMealThumb}" class="popup-img" alt="${meal.strMeal}">
-        <h2>${meal.strMeal}</h2>
-        <form id="comment-form">
-          <label for="username">Username:</label>
-          <input type="text" id="username" name="username" required><br><br>
-          <label for="comment">Comment:</label>
-          <textarea id="comment" name="comment" required></textarea><br><br>
-          <button type="submit" class="btn btn-success">Submit</button>
-        </form>
-        <h3 id="comment-title">Comments: (0)</h3>
-        <div id="comment-section"></div>
-        <button class="btn btn-danger" onclick="closePopup()">&times;</button>
-      </div>
-    </div>
-  `;
-
-  document.body.innerHTML += popupHTML;
-};
-
-window.closePopup = function closePopup() {
-  const popup = document.querySelector('.popup');
-  popup.parentNode.removeChild(popup);
-};
-
 async function display() {
   await received();
   window.openPopup = openPopup;
